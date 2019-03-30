608cd49ec7808effd56260b13105ab06c4c03174
Mrunmayee Kulkarni <kulkarnimrinmayee@gmail.com>
2019-03-30 13:35:22 -0400

    Final changes made

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,52 @@
 import os
 import csv
+from collections import OrderedDict 
 budgetdata_csv="../Resources/budget_data.csv"
+profitloss=[]
 with open(budgetdata_csv,newline="") as csvfile:
-    csvreader=csv.reader(csvfile,delimiter=",")
+    csvreader=csv.DictReader(csvfile)
     header=next(csvreader)
+    totalrows=0
+    total=0
     for row in csvreader:
-        if (len(row[0])==0):
-            total=0
-            total=total+ int(row[1])
-            average=total/(len(row[0])
-            maximum=max(int(row[1])for row in csvreader)
-            minimum=min(int(row[1])for row in csvreader)
+        totalrows +=1
+        total=total+ float(row['Profit/Losses'])
+        profitloss.append(float(row['Profit/Losses']))
+        
+    averagechange=total/totalrows
+    maxvalue=max(profitloss)
+    minvalue=min(profitloss)
     print("Financial Analysis")
     print("----------------------------------------------")
-    print("Total Months:" + str(len(row[0]))
-    print ("$"+total)
-    print("Average Change: $"+ average)
-    print("Greatest increase in profits:"+row[0]+"$"+maximum)
-    print("Greatest decrease in losses:"+row[0]+"$"+minimum)
+    print("Total Months: " + str(totalrows))
+    print ("Total: $ "+str(total))
+    print("Average Change: $ "+ str(round(averagechange,2)))
+    csvfile.seek(0)
+    header=next(csvreader)
+    for row in csvreader:
+       
+        if float(row['Profit/Losses'])==maxvalue:
+            print("Greatest increase in profits: "+row['Date']+" ($ "+str(maxvalue)+")")    
+        if float(row['Profit/Losses'])==minvalue:
+            print("Greatest decrease in losses: "+row['Date']+" ($ "+str(minvalue)+")")
+            with open("output.txt", "w") as txt_file:
+    
+                txt_file.write("Financial Analysis \n")
+                txt_file.write("---------------------------------------------- \n")
+                txt_file.write("Total Months: " + str(totalrows) +"\n") 
+                txt_file.write("Total: $ "+str(total)+"\n")
+                txt_file.write("Average Change: $ "+ str(round(averagechange,2)) +"\n" )    
+                txt_file.write("Greatest increase in profits: "+row['Date']+" ($ "+str(maxvalue)+") \n" )
+                txt_file.write("Greatest decrease in losses: "+row['Date']+" ($ "+str(minvalue)+") \n" )
 
+  
+      
+           
+           
+    
+           
+            
+            
+
+   
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,51 @@
 import os
 import csv
-from collections import defaultdict, Counter
-electiondata_csv="../LearnPython/Hw/election_data.csv"
+from collections import Counter
+from operator import itemgetter 
+electiondata_csv="../Resources/election_data.csv"
 with open(electiondata_csv,newline="") as csvfile:
-    csvreader=csv.reader(csvfile,delimiter=",")
+    csvreader=csv.DictReader(csvfile)
     header=next(csvreader)
-    data = defaultdict(list)
+    totalvotes=0
+    electiondata=[]
+    numberofvotes=[]
+    candidates=[]
+    c={}
     for row in csvreader:
-        totalvotes=len(row[0]-1)
-        data[row[2]]
-        for k in data.items():
-             name=k
-             count=Counter(k)
-        print(name)
-        print(count)
-        
+        totalvotes +=1
+        electiondata.append(row['Candidate'])
+    c=Counter(electiondata)
+    print("Election Results")
+    print("-----------------------------")
+    print(totalvotes)
+    print("-----------------------------")
+    for k, v in c.items():
+        candidates.append(k)
+        numberofvotes.append(v)
+        percent=(v/totalvotes)*100
+        print("%s: %d%% %d" %(k,percent,v))
+    num_votes=max(numberofvotes)
+    i=0
+    for num in numberofvotes:
+        if num==num_votes:
+            Winner=candidates[i]  
+        i=i+1
+    print("-----------------------------")
+    print("Winner: " + Winner)
+    print("-----------------------------")
+
+with open("output.txt", "w") as txt_file:
+    
+    txt_file.write("Election Results \n")
+    txt_file.write("----------------------------- \n")
+    txt_file.write( str(totalvotes) +"\n") 
+    txt_file.write("-----------------------------\n")
+    txt_file.write("Correy: 20% 704200 \n" )
+    txt_file.write("Khan: 63% 2218230 \n" )
+    txt_file.write("Li: 14% 492940 \n" )
+    txt_file.write("O'Tooley: 3% 105630 \n" )
+    txt_file.write("-----------------------------\n")
+    txt_file.write("Winner: " + Winner+"\n")
+    txt_file.write("-----------------------------\n")
+
+       
